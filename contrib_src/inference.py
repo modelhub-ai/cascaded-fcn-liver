32a7360eece58fa83ac2673d903a0d0a93c95c87
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-10-31 16:41:55 -0400

    misc changes

--- contrib_src/inference.py
+++ contrib_src/inference.py
@@ -14,26 +14,16 @@ class Model(ModelBase):
         # get the image processor
         self._imageProcessor = ImageProcessor(config)
         # load the DL model
-        self._model = caffe.Net("model/step1/step1_deploy.prototxt", 
-                                "model/step1/step1_weights.caffemodel", 
+        self._model = caffe.Net("model/step1/step1_deploy.prototxt",
+                                "model/step1/step1_weights.caffemodel",
                                 caffe.TEST)
-    
 
     def infer(self, input):
         # load preprocessed input
-        inputAsNpArr = self._imageProcessor.loadAndPreprocess(self._getDicomFilename(input))
+        inputAsNpArr = self._imageProcessor.loadAndPreprocess(input)
         # Run inference
         self._model.blobs['data'].data[...] = inputAsNpArr
         results = self._model.forward()
         # postprocess results into output
         output = self._imageProcessor.computeOutput(results)
         return output
-
-
-    def _getDicomFilename(self, inputFilename):
-        path, filename = os.path.split(inputFilename)
-        filename, _ = os.path.splitext(filename)
-        dicomFilename = str(os.path.join(path, "dicom", filename))
-        return dicomFilename
-
-
